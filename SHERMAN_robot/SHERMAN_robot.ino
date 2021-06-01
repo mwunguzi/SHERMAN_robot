84ec71bc7daad666151d2d34c29577ea76ae3781
Herve_Mwunguzi <mwunguziher@gmail.com>
2021-06-01 14:16:02 -0500

    fixing the line to calculate the intensity of the joystick

--- SHERMAN_robot/SHERMAN_robot.ino
+++ SHERMAN_robot/SHERMAN_robot.ino
@@ -1,6 +1,7 @@
+
 uint8_t swtchbutton=8;
-volatile uint8_t bttnRead,ENA=6,ENB=5,IN1=48,IN2=46,IN3=52,IN4=50;
-volatile uint16_t x,y;
+uint8_t bttnRead,ENA=6,ENB=5,IN1=48,IN2=46,IN3=52,IN4=50;
+double x,y;
 
 
 //function of driving a robot forward
@@ -66,18 +67,28 @@ float convertXYtoAngle(uint16_t a,uint16_t b){
 
  float x_cord,y_cord,angle;
  
- y_cord = map(a,0,1023,-512,512);
- x_cord = map(b,0,1023,-512,512);
+ y_cord = map(a,0,1023,-512,512)-2;
+ x_cord = map(b,0,1023,-512,512)+1;
+ Serial.print(" x_cordinate= ");
+ Serial.println(x_cord);
+ Serial.print("Y_cordinate= ");
+ Serial.println(y_cord);
  angle=atan2(y_cord,x_cord)*(180/PI);// obtaining the angle in degrees
- return angle;
+ return floor(angle);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-float inte_joystick;
+double inte_joystick;
+double red_x,red_y;
 x=analogRead(A0);
 y=analogRead(A1);
-inte_joystick=sqrt(pow(abs(x-512),2),pow(abs(y-512),2)); //reading the intensity on the joystock
+
+//reduced x because the center value is (500,500) and absolute function doesn't like calculation inside abs
+red_x = (x-514);
+red_y = (y-512);
+
+inte_joystick=pow((red_x*red_x) + (red_y*red_y),0.5); //reading the intensity on the joystock
 bttnRead=digitalRead(swtchbutton);
 Serial.println("X:  Y:  Button:");
 Serial.print(x);
@@ -89,5 +100,5 @@ Serial.print("angle= ");
 Serial.println(convertXYtoAngle(x,y));
 Serial.print("intensity on the joystick= ");
 Serial.println(inte_joystick);
-
+delay(1000);
 }
