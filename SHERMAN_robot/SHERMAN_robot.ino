4bf8e516797796e865d85430633c991171f5a8cc
Herve_Mwunguzi <mwunguziher@gmail.com>
2021-06-04 11:58:01 -0500

    fixed turning left with equivalent speed when turning right

--- SHERMAN_robot/SHERMAN_robot.ino
+++ SHERMAN_robot/SHERMAN_robot.ino
@@ -10,163 +10,163 @@ RF24 radio(7, 8);  // CE, CSN
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
-uint8_t swtchbutton=8;
-uint8_t bttnRead,ENA=6,ENB=5,IN1=48,IN2=46,IN3=24,IN4=22;
-double x,y,previousNum=0;
+uint8_t swtchbutton = 8;
+uint8_t bttnRead, ENA = 6, ENB = 5, IN1 = 48, IN2 = 46, IN3 = 24, IN4 = 22;
+double x, y, previousNum = 0;
 
 
 //function of driving a robot forward
-void driveForward(uint8_t speeed){
+void driveForward(uint8_t speeed) {
 
   //setting the motor speed
-  analogWrite(ENA,speeed);
-  analogWrite(ENB,speeed);
-  
+  analogWrite(ENA, speeed);
+  analogWrite(ENB, speeed);
+
   //making initial 1 and initial 4 high to move forward
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
 //function of driving the robot backward
-void driveBackward(uint8_t speeed){
+void driveBackward(uint8_t speeed) {
 
   //setting the motor speed
-  analogWrite(ENA,speeed);
-  analogWrite(ENB,speeed);
-  
+  analogWrite(ENA, speeed);
+  analogWrite(ENB, speeed);
+
   //making initial 2 and initial 3 high to move backward
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
 //function for turning right
-void turningRight(uint8_t speedl,uint8_t speedr){
+void turningRight(uint8_t speedl, uint8_t speedr) {
 
   //setting the motor speed
-  analogWrite(ENA,speedl);
-  analogWrite(ENB,speedr);
-  
+  analogWrite(ENA, speedl);
+  analogWrite(ENB, speedr);
+
   //making initial 2 and initial 3 high to move backward
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
 
 //this is a function that input the value of the joystick and tell which way it's heading
-float convertXYtoAngle(uint16_t a,uint16_t b){
+float convertXYtoAngle(uint16_t a, uint16_t b) {
 
- float x_cord,y_cord,angle;
- 
- y_cord = map(a,0,1023,-512,512)-2;
- x_cord = map(b,0,1023,-512,512)+1;
-// Serial.print(" x_cordinate= ");
-// Serial.println(x_cord);
-// Serial.print("Y_cordinate= ");
-// Serial.println(y_cord);
- angle=atan2(y_cord,x_cord)*(180/PI);// obtaining the angle in degrees
- return floor(angle);
+  float x_cord, y_cord, angle;
+
+  y_cord = map(a, 0, 1023, -512, 512) - 2;
+  x_cord = map(b, 0, 1023, -512, 512) + 1;
+  // Serial.print(" x_cordinate= ");
+  // Serial.println(x_cord);
+  // Serial.print("Y_cordinate= ");
+  // Serial.println(y_cord);
+  angle = atan2(y_cord, x_cord) * (180 / PI); // obtaining the angle in degrees
+  return floor(angle);
 }
 
-//this is a function to control the change of the intensity on the joystick 
-double change_when_new_number(double num){
-  
+//this is a function to control the change of the intensity on the joystick
+double change_when_new_number(double num) {
+
   double returnNum;
-  
-  if(previousNum >= num or previousNum < num){
-    
-    returnNum=previousNum-num;
-    previousNum=num;
+
+  if (previousNum >= num or previousNum < num) {
+
+    returnNum = previousNum - num;
+    previousNum = num;
     return returnNum;
-    
+
   }
-  
+
 }
 
 //this is a function to drive the robot in response to the joystick movement
-void mainDrive(float input_angle,double input_inte_joystick){
+void mainDrive(float input_angle, double input_inte_joystick) {
 
-  double inputSpeedA,inputSpeedB; //This will be the values for the input to controll how fast the motors are moving
+  double inputSpeedA, inputSpeedB; //This will be the values for the input to controll how fast the motors are moving
   double changeInJoystick;
-  
+
   //checking the the angle of the joystick is in the first or the second Quadrant
-  if(input_angle>1){
+  if (input_angle > 1) {
 
-  changeInJoystick=change_when_new_number(input_inte_joystick);
-  inputSpeedA=40;
-  inputSpeedB=(40+(-30*cos((input_angle*PI/180))));
-  
+    changeInJoystick = change_when_new_number(input_inte_joystick);
+    inputSpeedA = 40 + (30 * cos((input_angle * PI / 180)));
+    inputSpeedB = (40 + (-30 * cos((input_angle * PI / 180))));
 
-  //setting the motor speed
-  analogWrite(ENB,inputSpeedB);
-  analogWrite(ENA,inputSpeedA);
-  
-// Serial.print(" speed A: ");
-// Serial.println(inputSpeedA);
-// Serial.print("speed B ");
-// Serial.println(inputSpeedB);
-// Serial.print("joystick: ");
-// Serial.println(change_when_new_number(input_inte_joystick));
- 
-  //making initial 1 and initial 4 high to move forward
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH);
+
+    //setting the motor speed
+    analogWrite(ENB, inputSpeedB);
+    analogWrite(ENA, inputSpeedA);
+
+    // Serial.print(" speed A: ");
+    // Serial.println(inputSpeedA);
+    // Serial.print("speed B ");
+    // Serial.println(inputSpeedB);
+    // Serial.print("joystick: ");
+    // Serial.println(change_when_new_number(input_inte_joystick));
+
+    //making initial 1 and initial 4 high to move forward
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
   }
-  else if(input_angle<1 and input_angle!= 0){ // condition to make the robot move backwards
+  else if (input_angle < 1 and input_angle != 0) { // condition to make the robot move backwards
 
-    changeInJoystick=change_when_new_number(input_inte_joystick);
-    inputSpeedA=40;
-    inputSpeedB=(40+(-30*cos((input_angle*PI/180))));
-  
+    changeInJoystick = change_when_new_number(input_inte_joystick);
+    inputSpeedA = 40 + (30 * cos((input_angle * PI / 180)));
+    inputSpeedB = (40 + (-30 * cos((input_angle * PI / 180))));
 
-  //setting the motor speed
-  analogWrite(ENB,inputSpeedB);
-  analogWrite(ENA,inputSpeedA);
 
-  //making initial 2 and initial 3 high to move backward
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
-  
+    //setting the motor speed
+    analogWrite(ENB, inputSpeedB);
+    analogWrite(ENA, inputSpeedA);
+
+    //making initial 2 and initial 3 high to move backward
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+
   }
-  if((input_angle>=0 and input_angle<2) and input_inte_joystick<2){
+  if ((input_angle >= 0 and input_angle < 2) and input_inte_joystick < 2) {
 
-    analogWrite(ENB,0);
-    analogWrite(ENA,0);
-    
+    analogWrite(ENB, 0);
+    analogWrite(ENA, 0);
+
   }
 }
 
 
 void setup() {
   // put your setup code here, to run once:
-//Serial.begin(9600);
-//pinMode(swtchbutton,INPUT); // INTITIALIZING THE PRESS BUTTON ON THE JOYSTIC AS INPUT
+  //Serial.begin(9600);
+  //pinMode(swtchbutton,INPUT); // INTITIALIZING THE PRESS BUTTON ON THE JOYSTIC AS INPUT
 
-//Initializing the motor pins as output
-pinMode(ENA,OUTPUT);
-pinMode(ENB,OUTPUT);
-pinMode(IN1,OUTPUT);
-pinMode(IN2,OUTPUT);
-pinMode(IN3,OUTPUT);
-pinMode(IN4,OUTPUT);
+  //Initializing the motor pins as output
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
-while (!Serial);
+  while (!Serial);
 
-   Serial.begin(9600);
- 
+  Serial.begin(9600);
+
   radio.begin();
- 
+
   //set the address
   radio.openReadingPipe(0, address);
- 
+
   //Set module as receiver
   radio.startListening();
 
@@ -176,22 +176,22 @@ while (!Serial);
 
 void loop() {
   // put your main code here, to run repeatedly:
-double inte_joystick;
-double red_x,red_y;
+  double inte_joystick;
+  double red_x, red_y;
 
-//Read the data if available in buffer
+  //Read the data if available in buffer
   if (radio.available())
   {
     char joystickPos[10];
     radio.read(&joystickPos, sizeof(joystickPos));
-//    Serial.print("x, y: ");
-//    Serial.println(joystickPos);
+    //    Serial.print("x, y: ");
+    //    Serial.println(joystickPos);
     //delay(1000);
 
-  //parsing the joystickPos string to obtain the x and y joystick positions
-  const char delimiter[] = ","; //the character to look for when parsing
-  char parsedStrings[2][5]; //char array to store parsed strings
-  char *token =  strtok(joystickPos, delimiter); //diving the string at ","
+    //parsing the joystickPos string to obtain the x and y joystick positions
+    const char delimiter[] = ","; //the character to look for when parsing
+    char parsedStrings[2][5]; //char array to store parsed strings
+    char *token =  strtok(joystickPos, delimiter); //diving the string at ","
     strncpy(parsedStrings[0], token, sizeof(parsedStrings[0]));//first one
     for (int i = 1; i < 2; i++) {
       token =  strtok(NULL, delimiter);
@@ -199,30 +199,30 @@ double red_x,red_y;
     }
 
     //for (int i = 0; i < 2; i++){
-      //Serial.println(parsedStrings[i]);}//  should have the 2 data strings parsed o
-     x = atoi(parsedStrings[0]);
-     y = atoi(parsedStrings[1]);
+    //Serial.println(parsedStrings[i]);}//  should have the 2 data strings parsed o
+    x = atoi(parsedStrings[0]);
+    y = atoi(parsedStrings[1]);
   }
-  
 
 
-//reduced x because the center value is (500,500) and absolute function doesn't like calculation inside abs
-red_x = (x-514);
-red_y = (y-511);
 
-inte_joystick=pow((red_x*red_x) + (red_y*red_y),0.5); //reading the intensity on the joystock
-bttnRead=digitalRead(swtchbutton);
-//Serial.println("X:  Y:  Button:");
-//Serial.print(x);
-//Serial.print("  ");
-//Serial.print(y);
-//Serial.print("  ");
-//Serial.println(bttnRead);
-//Serial.print("angle= ");
-//Serial.println(convertXYtoAngle(x,y));
-//Serial.print("intensity on the joystick= ");
-//Serial.println(inte_joystick);
+  //reduced x because the center value is (500,500) and absolute function doesn't like calculation inside abs
+  red_x = (x - 514);
+  red_y = (y - 511);
 
-mainDrive(convertXYtoAngle(x,y),inte_joystick);
+  inte_joystick = pow((red_x * red_x) + (red_y * red_y), 0.5); //reading the intensity on the joystock
+  bttnRead = digitalRead(swtchbutton);
+  //Serial.println("X:  Y:  Button:");
+  //Serial.print(x);
+  //Serial.print("  ");
+  //Serial.print(y);
+  //Serial.print("  ");
+  //Serial.println(bttnRead);
+  //Serial.print("angle= ");
+  //Serial.println(convertXYtoAngle(x,y));
+  //Serial.print("intensity on the joystick= ");
+  //Serial.println(inte_joystick);
+
+  mainDrive(convertXYtoAngle(x, y), inte_joystick);
 
 }
